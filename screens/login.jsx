005782f19d3147b29980f97b5a84496818913a15
LaptopIsrael\jader <rios.jader.20.04@gmail.com>
2024-09-25 19:37:12 -0500

    proyecto

---
+++ screens/login.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function login() {
+  return (
+    <View className='bg'>
+      <Text>login</Text>
+    </View>
+  );
+}
